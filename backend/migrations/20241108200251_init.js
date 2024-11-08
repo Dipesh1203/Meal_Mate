62c1651e07fc6d618cced828062ab663946b22e0
[Eklavya Singh] <[eklavyasinghparihar7875@gmail.com]>
2024-11-09 01:52:02 +0530

    added database

---
+++ backend/migrations/20241108200251_init.js
@@ -0,0 +1,25 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("user", (table) => {
+    table.increments("id").primary();
+    table.string('email').notNullable()
+    table.string('phone').notNullable()
+    table.string("entity_name").notNullable().unique();
+    table.text("address").notNullable();
+    table.text("latitude");
+    table.text("longitude");
+    table.text("legal_identity");
+    table.enum("entity", ["NGO", "PROVIDER"]);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable("users");
+};
