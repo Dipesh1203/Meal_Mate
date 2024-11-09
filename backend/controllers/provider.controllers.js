cb4d7a4ba8194e1e9449738bcadc3613bd1d0ed0
Dipesh Prajapat <89146867+Dipesh1203@users.noreply.github.com>
2024-11-09 19:55:27 +0530

    Upadated migration and add new field in donation_meal (#7)

--- backend/controllers/provider.controllers.js
+++ backend/controllers/provider.controllers.js
@@ -5,6 +5,7 @@ module.exports.createDonationMeal = async (req, res) => {
     donor_id,
     meal_description,
     quantity,
+    provider_name,
     pickup_location,
     latitude,
     longitude,
@@ -13,7 +14,7 @@ module.exports.createDonationMeal = async (req, res) => {
     is_claimed,
     claimed_by,
   } = req.body;
-  
+
   // Validate required fields
   if (
     !donor_id ||
@@ -24,23 +25,26 @@ module.exports.createDonationMeal = async (req, res) => {
   ) {
     return res.status(400).json({ error: "Required fields are missing" });
   }
-  
+
   console.log(req.body);
   try {
     // Insert the new donation meal into the database
-    const newMealId = await db("donation_meal")
-      .insert({donor_id,
+    const [newMealId] = await db("donation_meal")
+      .insert({
+        donor_id,
         meal_description,
         quantity,
         pickup_location,
         latitude,
         longitude,
+        provider_name,
         expiry_date,
         pickup_time: pickup_time || knex.fn.now(), // Default to current time if not provided
         is_claimed: is_claimed || false,
         claimed_by: claimed_by || null,
-      });
-      
+      })
+      .returning("*");
+
     res.status(201).json({
       message: "Donation meal created successfully",
       donation_meal_id: newMealId,
