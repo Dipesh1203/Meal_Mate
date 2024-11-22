3d23bfd091037486a5d571450b594c3d23dbf9ef
Dipesh1203 <prajapatdipesh86@gmail.com>
2024-11-22 11:12:49 +0530

    Fix:Final api fixes 2

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: `${import.meta.env.VITE_API}`, // Use environment variable with VITE_ prefix
+        target: `${process.env.VITE_API}`, // Use environment variable with VITE_ prefix
         rewrite: (path) => {
           return path.replace(/^\/api/, "");
         },
